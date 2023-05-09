eed03539326379a7899ede75664260412ab45549
Benjamin Pinter <72715672+BenjaminDavidPinter@users.noreply.github.com>
2023-05-08 23:05:08 -0400

    Orbitals

--- app.js
+++ app.js
@@ -9,27 +9,81 @@ function setup() {
             x: window.innerWidth / 2,
             y: window.innerHeight / 2,
             size: 0.00,
-            sizeMax: 50,
-            growthRate: .15,
-            color: 'blue',
-            draw: standardDraw
+            sizeMax: 75,
+            growthRate: .50,
+            color: 'yellow',
+            draw: centerOrbital
         },
         {
             name: "Ideas",
-            x: window.innerWidth / 2 + Math.floor(Math.random() * 100) + 200,
-            y: window.innerHeight / 2 + Math.floor(Math.random() * 100) + 200,
+            x: 0,
+            y: 0,
+            angle: 200,
             size: 0.00,
             sizeMax: 25,
             growthRate: .15,
             color: 'red',
-            draw: standardDraw
+            draw: childOrbital,
+            ellipse: {
+                x: this.window.innerWidth / 2,
+                y: this.window.innerHeight / 2,
+                w: 600,
+                h: 150
+            }
+        },
+        {
+            name: "About Me",
+            x: 0,
+            y: 0,
+            angle: 115,
+            size: 0.00,
+            sizeMax: 25,
+            growthRate: .15,
+            color: 'green',
+            draw: childOrbital,
+            ellipse: {
+                x: this.window.innerWidth / 2,
+                y: this.window.innerHeight / 2,
+                w: 600,
+                h: 100
+            }
+        },
+        {
+            name: "Skills",
+            x: 0,
+            y: 0,
+            angle: 60,
+            size: 0.00,
+            sizeMax: 25,
+            growthRate: .15,
+            color: 'purple',
+            draw: childOrbital,
+            ellipse: {
+                x: this.window.innerWidth / 2,
+                y: this.window.innerHeight / 2,
+                w: 400,
+                h: 200
+            }
         }
     ];
+
+    stars = {
+        collection: [],
+        alpha: 0
+    };
+
+    for (let i = 0; i < 1000; i++) {
+        stars.collection[i] = {
+            x: Math.floor(Math.random() * window.innerWidth),
+            y: Math.floor(Math.random() * window.innerHeight)
+        }
+    }
 }
 
 function draw() {
     if (beginDraw) {
-        background(240, 255, 240);
+        background(0, 0, 0);
+        drawStars();
         circles.forEach(element => {
             element.draw();
         });
@@ -62,6 +116,16 @@ function ditchSplash() {
 
 }
 
+let drawStars = () => {
+    stars.collection.forEach(star => {
+        fill(`rgba(255, 255, 255, ${stars.alpha})`);
+        circle(star.x, star.y, 2);
+    });
+    if (stars.alpha < 1) {
+        stars.alpha += .005;
+    }
+}
+
 function fadeElementOut(fadeTarget) {
     var fadeEffect = setInterval(function () {
         if (!fadeTarget.style.opacity) {
@@ -75,7 +139,25 @@ function fadeElementOut(fadeTarget) {
     }, 10);
 }
 
-function standardDraw() {
+function childOrbital() {
+    fill(this.color);
+    if (this.size < this.sizeMax) {
+        this.size += this.growthRate;
+    }
+    this.x = Math.sin(this.angle) * (this.ellipse.w / 2) + (window.innerWidth / 2);
+    this.y = Math.cos(this.angle) * (this.ellipse.h / 2) + (window.innerHeight / 2);
+    circle(this.x, this.y, this.size);
+    //stroke(this.color)
+    noFill();
+    //ellipse(this.ellipse.x, this.ellipse.y, this.ellipse.w, this.ellipse.h);
+    if (this.angle > 359) {
+        this.angle = 0;
+    } else {
+        this.angle += 0.001;
+    }
+}
+
+function centerOrbital() {
     fill(this.color);
     if (this.size < this.sizeMax) {
         this.size += this.growthRate;
